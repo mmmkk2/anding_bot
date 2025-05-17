8076c87708c2157c1e4d6efaa681ab70f8f8b06a
Mikyung Shim <anding.mkshim@gmail.com>
2025-05-17 19:45:48 +0900

    `

--- check_module_payment.py
+++ check_module_payment.py
@@ -209,7 +209,7 @@ def save_payment_dashboard_html(payments):
             }}
             th, td {{
                 border: 1px solid #dee2e6;
-                padding: 0.3rem;
+                padding: 0.1rem;
             }}
             th {{
                 background-color: #6c757d;
@@ -219,6 +219,7 @@ def save_payment_dashboard_html(payments):
                 background-color: #f8f9fa;
             }}
             .breakable {{
+                padding: 0.3rem;
                 word-break: break-word;
                 white-space: normal;
                 max-width: 80px;
