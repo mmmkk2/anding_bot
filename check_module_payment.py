4234b9dfe686513f3ff46421e47340f7ddb7a213
Mikyung Shim <anding.mkshim@gmail.com>
2025-05-17 19:47:22 +0900

    1

--- check_module_payment.py
+++ check_module_payment.py
@@ -219,7 +219,7 @@ def save_payment_dashboard_html(payments):
                 background-color: #f8f9fa;
             }}
             .breakable {{
-                padding: 0.3rem;
+                padding: 0.4rem;
                 word-break: break-word;
                 white-space: normal;
                 max-width: 80px;
