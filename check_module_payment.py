cce277b29b76864eb04fdea3d599584cc33fa9ff
Mikyung Shim <anding.mkshim@gmail.com>
2025-05-17 18:58:49 +0900

    wrap

--- check_module_payment.py
+++ check_module_payment.py
@@ -83,7 +83,7 @@ def check_payment_status(driver):
             status = cols[5].text.strip()          # 결제상태 (예: 승인완료)
             amount = cols[6].text.strip()          # 결제금액
             payment_date = cols[7].text.strip()    # 결제일시
-            seat_type = cols[8].text.strip().split("/")[0] + "<br>" + cols[9].text.strip()      # 결제상품 (예: 스터디룸(2인) 등)
+            seat_type = cols[8].text.strip().split("/")[0] + " " + cols[9].text.strip()      # 결제상품 (예: 스터디룸(2인) 등)
             
 
             # cols[9]는 시작시간, cols[10]는 종료시간, cols[11]는 가입일
@@ -149,7 +149,7 @@ def save_payment_dashboard_html(payments):
                 <td>{row['user']}</td>
                 <td>{row['amount']}</td>
                 <td>{row['seat_type']}</td>
-                <td>{row['date'][:10]}<br>{row['date'][11:19]}</td>
+                <td>{row['date'][:10]} {row['date'][11:19]}</td>
             </tr>
         """
 
@@ -209,6 +209,10 @@ def save_payment_dashboard_html(payments):
             th, td {{
                 border: 1px solid #dee2e6;
                 padding: 0.1rem;
+            }}
+            td {{
+                word-break: break-word;
+                white-space: normal;
             }}
             th {{
                 background-color: #6c757d;
