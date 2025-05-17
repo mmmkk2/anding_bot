42898039ff11a997d1976e1f72f8a6ed0fc6b965
Mikyung Shim <anding.mkshim@gmail.com>
2025-05-17 20:08:57 +0900

    1

--- check_module_payment.py
+++ check_module_payment.py
@@ -223,7 +223,7 @@ def save_payment_dashboard_html(payments):
                 padding: 0.4rem;
                 word-break: break-word;
                 white-space: normal;                
-                max-width: 120px; /* ✅ 줄바꿈을 유도하는 제한 (선택사항) */
+                max-width: 80px; /* ✅ 줄바꿈을 유도하는 제한 (선택사항) */
             }}
         </style>
     </head>
