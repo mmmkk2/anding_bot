aead8a0df030708ba7dc1c4b33f77b1a2aba39b3
Mikyung Shim <anding.mkshim@gmail.com>
2025-05-18 19:16:58 +0900

    1

--- dashboard_seat.py
+++ dashboard_seat.py
@@ -321,13 +321,11 @@ def save_seat_dashboard_html(used_free, total_free, used_laptop, total_laptop, r
                 margin-top: 1rem;
             }}          
             @media (max-width: 480px) {{
-            body {{
-                max-width: 90vw;
-            }}
+                body {{
+
+                }}
                 .box {{
-                max-width: 95vw;
-                /* padding: 1.2rem;
-                max-height: 600;  /* 화면 높이의 90%까지 확장 */
+                    max-height: 100vh;  /* 화면 높이의 90%까지 확장 */
                 }}
             }}                
         </style>
