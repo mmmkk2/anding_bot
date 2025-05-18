2954e525dc57d3adb49ed321adc0422b4393877c
Mikyung Shim <anding.mkshim@gmail.com>
2025-05-18 21:55:52 +0900

    1

--- dashboard_seat.py
+++ dashboard_seat.py
@@ -338,7 +338,7 @@ def save_seat_dashboard_html(used_free, total_free, used_laptop, total_laptop, r
             <div class="stat">남은 자유석: {remaining}석</div>            
             <div class="updated">업데이트 시각: {now_str}</div>
             <div style="margin-top:0.5rem;">            
-                 <canvas id="seatChart" height="200"></canvas>
+                 <canvas id="seatChart" style="height: 30vh; width: 100%;"></canvas>
                 {chart_script}
             </div>
         </div>
