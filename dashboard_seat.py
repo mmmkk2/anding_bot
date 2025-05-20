5a1955588286776248d72931439a68f74061e4b4
Mikyung Shim <anding.mkshim@gmail.com>
2025-05-20 19:10:41 +0900

    @media (max-width: 480px) {{                 .box {{                     max-height: 90vh;  /* 화면 높이의 90%까지 확장 */                 }}             }}

--- dashboard_seat.py
+++ dashboard_seat.py
@@ -352,7 +352,7 @@ def save_seat_dashboard_html(used_free, total_free, used_laptop, total_laptop, r
             }}          
             @media (max-width: 480px) {{
                 .box {{
-                    max-height: 100vh;  /* 화면 높이의 90%까지 확장 */
+                    max-height: 90vh;  /* 화면 높이의 90%까지 확장 */
                 }}
             }}                
         </style>
