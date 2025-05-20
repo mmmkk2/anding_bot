63910440b105ac350d568dba5a42f73e5b177608
Mikyung Shim <anding.mkshim@gmail.com>
2025-05-20 19:49:08 +0900

    max-height: 250px;

--- dashboard_seat.py
+++ dashboard_seat.py
@@ -315,8 +315,8 @@ def save_seat_dashboard_html(used_free, total_free, used_laptop, total_laptop, r
                 margin: 0;
                 display: flex;
                 align-items: flex-start;
-                min-height: 300px; /* max-height: 25vh; */
-                max-height: 300px; /*   max-width: 100vw; */ 
+                min-height: 250px; /* max-height: 25vh; */
+                max-height: 250px; /*   max-width: 100vw; */ 
                 box-sizing: border-box;
                 justify-content: center;
                 text-align: center;  /* 텍스트 정렬 보정 */               
