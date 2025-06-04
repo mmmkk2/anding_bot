9f94bdd7b131ec2b093461d3683ff9a4f024e21e
Mikyung Shim <anding.mkshim@gmail.com>
2025-06-04 20:44:27 +0900

    render_table_expire, idx

--- module/dashboard_seat.py
+++ module/dashboard_seat.py
@@ -323,7 +323,7 @@ def render_table_expire(title, rows):
             <tbody>
     """
     for idx, (seat_type, seat_number, name, product, start_time, end_time) in enumerate(rows, 1):
-        html_table += f"<tr><td>{len(rows) - idx + 1}</td><td>{seat_number}</td><td>{name}</td><td>{product}</td><td class='time'>{end_time.replace('.', '-')}</td></tr>"
+        html_table += f"<tr><td>{idx}</td><td>{seat_number}</td><td>{name}</td><td>{product}</td><td class='time'>{end_time.replace('.', '-')}</td></tr>"
     html_table += """
             </tbody>
         </table>
