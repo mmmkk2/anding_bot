d9d037e186e1499288a817a3e5f6b34e9367b8c7
Mikyung Shim <anding.mkshim@gmail.com>
2025-06-07 02:03:29 +0900

    <div style="font-family: monospace; white-space: pre;">

--- module/dashboard_monthly.py
+++ module/dashboard_monthly.py
@@ -309,11 +309,12 @@ def fetch_monthly_sales_from_calendar(driver):
                     }});
                 </script>
                 <div class="summary-box">
-                    <div> 
-                    {prev_month}월 누적 매출 : {summary_amount_prev:,}원<br>
-                    {curr_month}월 누적 매출 : {summary_amount_curr:,}원<br>
-                    {curr_month}월 일평균 매출: {daily_avg:,}원<br>
-                    {curr_month}월 예측 매출: {predicted_amount:,}원</div>
+                    <div style="font-family: monospace; white-space: pre;">
+                    {curr_month}월 누적 매출   : {summary_amount_curr:,}원<br>
+                    {curr_month}월 일평균 매출 : {daily_avg:,}원<br>
+                    {curr_month}월 예측 매출   : {predicted_amount:,}원<br>
+                    {prev_month}월 누적 매출   : {summary_amount_prev:,}원
+                    </div>                    
                 </div>                
             </div> 
         <div class="updated">Updated {now_str}</div>
