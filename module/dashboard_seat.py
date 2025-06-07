0a2a3be71cc9ece7111f2a754e52153e7309a7ff
Mikyung Shim <anding.mkshim@gmail.com>
2025-06-07 21:06:37 +0900

    driver.execute_script("window.scrollTo(0, 100);")

--- module/dashboard_seat.py
+++ module/dashboard_seat.py
@@ -396,7 +396,7 @@ def main_check_seat():
                 time.sleep(2)
                 map_screenshot_path = os.path.join(DASHBOARD_PATH, "../static/images/seat_map.png")
                 os.makedirs(os.path.dirname(map_screenshot_path), exist_ok=True)
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight * 0.05);")
+                driver.execute_script("window.scrollTo(0, 100);")
                 time.sleep(0.5)
                 element = driver.find_element(By.CSS_SELECTOR, "div#store_map_container > div#store_map_wrap")
                 element.screenshot(map_screenshot_path)
