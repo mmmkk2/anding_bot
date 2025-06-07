918ec9918dd0a86a591737a2dc76274aeb0386b6
Mikyung Shim <anding.mkshim@gmail.com>
2025-06-08 01:04:12 +0900

    now_hour_kst = (datetime.utcnow().hour + 9) % 24  # KST 기준 시간     if now_hour_kst not in [0, 1/6]:

--- main_run_all.py
+++ main_run_all.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     run_and_log(main_check_product, "/home/mmkkshim/anding_bot/logs/run_product.log", label="상품 확인")
 
     now_hour_kst = (datetime.utcnow().hour + 9) % 24  # KST 기준 시간
-    if now_hour_kst not in [0, 1]:
+    if now_hour_kst not in [0, 1/6]:
         print("▶️ 결제 확인 시작")
         run_and_log(main_check_payment, "/home/mmkkshim/anding_bot/logs/run_p.log", label="결제 확인")
     else:
